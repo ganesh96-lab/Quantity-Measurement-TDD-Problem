dd19cd41114707c8dc3dce54efa70ecc96cdbca0
ganesh96-lab <ganeshghodke7050@gmail.com>
2020-04-21 12:20:25 -0400

    ADDED:Test to Type check

--- src/test/java/QuantityTest.java
+++ src/test/java/QuantityTest.java
@@ -21,4 +21,10 @@ public class QuantityTest {
         Feet feet2 = new Feet(0.0);
         Assert.assertNotSame(feet1,feet2);
     }
+
+    @Test
+    public void givenTwoDiffDataType_shouldReturnFalse() {
+        Feet feet2 = new Feet(0.0);
+        Assert.assertNotEquals("String",feet2);
+    }
 }
