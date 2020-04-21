b3854178c90989ab61f1fd431aa474ece613f6a7
ganesh96-lab <ganeshghodke7050@gmail.com>
2020-04-21 13:02:28 -0400

    ADDED:Test to equality of null with reference

--- src/test/java/QuantityTest.java
+++ src/test/java/QuantityTest.java
@@ -47,4 +47,11 @@ public class QuantityTest {
         Inch inch2=new Inch(0.0);
         Assert.assertEquals(inch1,inch2);
     }
+
+    @Test
+    public void givenNullAndObject_shouldNotEqual() {
+        Inch inch1=new Inch(0.0);
+        Inch inch2=null;
+        Assert.assertNotEquals(inch1,inch2);
+    }
 }
