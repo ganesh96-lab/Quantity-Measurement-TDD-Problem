1839e31ded223ae805971bfc16d7397e08009936
ganesh96-lab <ganeshghodke7050@gmail.com>
2020-04-21 12:43:54 -0400

    REFACTOR:Code to check same reference

--- src/test/java/QuantityTest.java
+++ src/test/java/QuantityTest.java
@@ -33,6 +33,11 @@ public class QuantityTest {
         Feet feet1=new Feet(0.0);
         Feet feet2 = new Feet(1.0);
         Assert.assertNotEquals(feet1,feet2);
+    }
 
+    @Test
+    public void givenSameReferance_shouldReturnEqual() {
+        Feet feet2 = new Feet(1.0);
+        Assert.assertEquals(feet2,feet2);
     }
 }
