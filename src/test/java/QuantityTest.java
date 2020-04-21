a39e18d354655ee4db3a4a42480ada5783e15035
ganesh96-lab <ganeshghodke7050@gmail.com>
2020-04-21 12:30:30 -0400

    Test to value check for equality

--- src/test/java/QuantityTest.java
+++ src/test/java/QuantityTest.java
@@ -27,4 +27,12 @@ public class QuantityTest {
         Feet feet2 = new Feet(0.0);
         Assert.assertNotEquals("String",feet2);
     }
+
+    @Test
+    public void given0FeetAnd1Feet_shouldReturnNotEqual() {
+        Feet feet1=new Feet(0.0);
+        Feet feet2 = new Feet(1.0);
+        Assert.assertNotEquals(feet1,feet2);
+
+    }
 }
