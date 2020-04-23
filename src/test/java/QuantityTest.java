a73c2ad8bf7e6087ece156e18889c1a89415a317
ganesh96-lab <ganeshghodke7050@gmail.com>
2020-04-22 23:28:31 -0400

    ADDED:Test to check 1 feet equal to 12 inch

--- src/test/java/QuantityTest.java
+++ src/test/java/QuantityTest.java
@@ -99,4 +99,12 @@ public class QuantityTest {
         boolean compareValue=feet.compare(inch);
         Assert.assertFalse(compareValue);        
     }
+
+    @Test
+    public void given1FeetAnd12Inch_shouldReturnEqual() {
+        Length feet=new Length(Length.Unit.FEET,1);
+        Length inch=new Length(Length.Unit.INCH,12);
+        boolean compareValue=feet.compare(inch);
+        Assert.assertTrue(compareValue);
+    }
 }
